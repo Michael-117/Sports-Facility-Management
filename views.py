64a762e6a4b744162456ddeddf1fa5e651a0293b
blacklunaris <patrickhowardanglin@gmail.com>
2021-03-23 22:49:57 -0500

    css adjustments

--- views.py
+++ views.py
@@ -106,7 +106,7 @@ def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
 
-
+app.config.from_object(__name__)
 if __name__ == '__main__':
     app.secret_key=os.urandom(12)
     app.run(debug=True, host="0.0.0.0", port="8080")
